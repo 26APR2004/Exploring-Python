da41c794c5482c3ffb2b0feb0ed27cd361c18c23
sujal26 <159209658+26APR2004@users.noreply.github.com>
2024-07-14 20:00:51 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,48 @@
+# Data Structre- List
+list=['me','nhi','tho','kon','be']
+
+# POP Operation
+list.pop()
+print(list)
+
+# Sort Function
+
+num=[2,5,3,1,7]
+num.sort()
+print(num)
+
+# Append Function
+
+num.append(15)
+print(num)
+
+# Remove Function
+
+num.remove(2)
+print(num)
+
+# Insert Function
+
+num.insert(2,3)
+print(num)
+
+# Count Function
+
+p=[1,1,2,2,3,3,3,3]
+p.count(3)
+print(p.count(3))
+
+# Clear Function
+
+p.clear()
+print(p)
+
+# Extend Function- means concatinate
+
+list.extend(num)
+print(list)
+
+# Reverse Function
+
+num.reverse()
+print(num)

178281d7317bd0d9f04e29d238bc5319df8c983e
sujal26 <159209658+26APR2004@users.noreply.github.com>
2024-07-15 23:51:42 +0530

    Add files via upload

---
+++ # String in Python.py
@@ -0,0 +1,16 @@
+# String in Python
+
+s="I love Python" # Declare string
+
+# string general operation
+print(s[0])
+print(s[2])
+print(s[3])
+print(s[4])
+print(s[5])
+
+# Slicing in string
+print(s[:6])
+print(s[7:])
+print(s[-1:-6])
+

4a1ba6643be71d296f2ebfecebc4ea00d6ef10a4
sujal26 <159209658+26APR2004@users.noreply.github.com>
2024-07-17 09:02:22 +0530

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,22 @@
+
+# Many value to Multiple variables
+
+x,y,z="Mango","orange","apple"
+print(x)
+print(y)
+print(z)
+
+# Multiple variable to one value
+
+x=y=z="HI"
+print(x)
+print(y)
+print(z)
+
+# Unpack a collection
+
+list=["Hello","hola","namste"]
+x,y,z=list
+print(x)
+print(y)
+print(z)

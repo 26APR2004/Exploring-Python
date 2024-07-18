8cd15f7e6beb701d10c9d030cb373eb445e1e099
sujal26 <159209658+26APR2004@users.noreply.github.com>
2024-07-18 22:03:19 +0530

    Add files via upload

---
+++ print.py
@@ -0,0 +1,17 @@
+
+# # Python print with end parameter
+# print("Hello man ",end='')
+# print("hello")
+
+#python print with sep parameter
+print("New year",2024,"happy new year",sep="!")
+
+#output formating
+x=12
+h=34
+print("The value of x is{} and value of h is{}".format(x,h))
+
+#user input
+num=int(input("Enter the value"))
+print(num)
+print(type(num))

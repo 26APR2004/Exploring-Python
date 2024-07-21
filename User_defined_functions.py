8aa988728d2b5890482701aa0f51dc7c3eb124ee
sujal26 <159209658+26APR2004@users.noreply.github.com>
2024-07-21 16:29:53 +0530

    Add files via upload

---
+++ User_defined_functions.py
@@ -0,0 +1,10 @@
+# User defined functions
+# Simple one Example
+
+def add(a,b):   # Function define
+    return a+b
+a=12
+b=23
+result=add(a,b)  # Function call
+print("Addition of {} and {} is:".format(a,b),result)
+

831750cebea2ab7e2fe3ed290a73b4c6042279ba
sujal26 <159209658+26APR2004@users.noreply.github.com>
2024-07-21 16:17:23 +0530

    Add files via upload

---
+++ Inbuilt_function.py
@@ -0,0 +1,21 @@
+# Inbuilt function
+
+List=[1,23,45,89,72]
+print("Largest number:",max(List))
+print("small number:",min(List))
+
+# If you want to take input then use input() inbuilt function
+
+x=input("Enter your age:")
+print("Your age is:",x)
+
+# Suppose you want to perform the operation in string form
+a=eval("2+3*5")
+print( "Answer:",a) 
+
+# Converstion of one data type into another data type
+l='a'
+g=2
+print(float(g))
+print(complex(g))
+print(ord(l))# ASCII converstion values
